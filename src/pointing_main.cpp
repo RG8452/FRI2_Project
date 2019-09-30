371d92c4b1f987bcb22ebc4bedf7e92165ad913f
jasoncox576 <jasongenecox@gmail.com>
2019-09-30 17:00:25 -0500

    Added function to subscribe to camera

--- src/pointing_main.cpp
+++ src/pointing_main.cpp
@@ -1,18 +1,43 @@
-#include "ros/ros.h"
-#include "fri2/test.h"
+#include <ros/ros.h>
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/image_encodings.h>
+#include <cv_bridge/cv_bridge.h>
 
-//Callback function, for sure not written correctly
+//#include "libfreenect.h"
+//#include "fri2/test.h"
+#include "fri2/ImageProcessor.h"
+
 /*
-void receiveImage(const std_msgs::StringConstPtr &msg) {
-	ROS_INFO(msg);
-}
-*/
+ * There's some weird jank going on here, I'm honestly just testing the kinect
+ * ImageProcessor supports string and encoding construction
+ * Encodings are std::strings found in image_encodings.h, as seen here:
+ * http://docs.ros.org/jade/api/sensor_msgs/html/image__encodings_8h_source.html
+ *
+ * Honestly this is just testing stuff but I want to see what all the images are
+ * Dr Hart claimed something in here should be hi-res, but all I'm finding is 480x640
+ */
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "hw5_node");
     ros::NodeHandle n;
 
-    //ros::Subscriber sub = n.subscribe("/camera/depth/points", 1000, receiveImage);
+    ImageProcessor ip("/camera/rgb/image_color");
+    ros::Subscriber sub = n.subscribe<sensor_msgs::Image>("/camera/rgb/image_color",
+                                                          100, &ImageProcessor::imageCb, &ip);
+
+    ImageProcessor ip2("/camera/rgb/image_rect_color");
+    ros::Subscriber sub2 = n.subscribe<sensor_msgs::Image>("/camera/rgb/image_rect_color",
+                                                          100, &ImageProcessor::imageCb, &ip2);
+
+    ImageProcessor ip3("/camera/depth/image", sensor_msgs::image_encodings::TYPE_32FC1);
+    ros::Subscriber sub3 = n.subscribe<sensor_msgs::Image>("/camera/depth/image",
+                                                          100, &ImageProcessor::imageCb, &ip3);
+
+    ImageProcessor ip4("/camera/depth/image_rect", sensor_msgs::image_encodings::TYPE_32FC1);
+    ros::Subscriber sub4 = n.subscribe<sensor_msgs::Image>("/camera/depth/image_rect",
+                                                          100, &ImageProcessor::imageCb, &ip4);
+
+    ros::spin();
 
     return 0;
 }
